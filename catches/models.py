c1d53fcaaa040b239d2069da6681ea7193442259
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-13 19:16:52 +0100

    Added catches base model and comments model.

--- catches/models.py
+++ catches/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Private"), (1, "Public"))
 
 # Create your models here.
+
+
+class Catches(models.Model):
+    """
+    Stores a single blog catch entry related to :model:`auth.User`.
+    """
+    fish_species = models.CharField(max_length=200)
+    fish_length = models.FloatField()
+    fish_weight = models.FloatField()
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="catch_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    public = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.fish_species} caught by {self.author}"
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`catches.Catches`.
+    """
+    post = models.ForeignKey(Catches, on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
