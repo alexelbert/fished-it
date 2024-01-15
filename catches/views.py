eace55d2edd1f9e9578daf9f91574129d361af06
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-15 19:03:00 +0100

    Added views function to display details from a single catch.

--- catches/views.py
+++ catches/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Catch
+from django.shortcuts import render, get_object_or_404
+
 
 class CatchesList(generic.ListView):
     queryset = Catch.objects.all()
     template_name = "catches/index.html"
     paginate_by = 6
 
-def catch_list(request):
-    # Retrieve all public catches
-    catches = Catch.objects.filter(public=1)
 
-    # Pass the catches to the template for rendering
-    return render(request, 'catches/catches_list.html', {'object_list': catches})
+def catch_detail(request, slug):
+    """
+    Display an individual :model:`catches.catch`.
+
+    **Context**
+
+    ``catch``
+        An instance of :model:`catches.catch`.
+
+    **Template:**
+
+    :template:`catches/catch_detail.html`
+    """
+
+    queryset = Catch.objects.filter(public=1)
+    catch = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "catches/catch_detail.html",
+        {"catch": catch},
+    )
