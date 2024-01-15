af4b505f5b1a5c8f745442ea5bb8b76db70150b5
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-15 14:41:57 +0100

    Enabled static file serving for heroku.

--- fished_it/settings.py
+++ fished_it/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 LOCAL_IP = os.environ.get("LOCAL_IP")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     LOCAL_IP,
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
