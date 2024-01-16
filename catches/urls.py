78deaf77e072a532d7fa880f2d9d0972eab9ad9c
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-16 17:11:27 +0100

    Added add_catch url.

--- catches/urls.py
+++ catches/urls.py
@@ -2,10 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.CatchesList.as_view(), name='home'),
-    path('<slug:slug>/', views.catch_detail, name='catch_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
-         views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
-         views.comment_delete, name='comment_delete'),
+     path('', views.CatchesList.as_view(), name='home'),
+     path('add_catch/', views.add_catch, name='add_catch'),
+     path('<slug:slug>/', views.catch_detail, name='catch_detail'),
+     path('<slug:slug>/edit_comment/<int:comment_id>',
+          views.comment_edit, name='comment_edit'),
+     path('<slug:slug>/delete_comment/<int:comment_id>',
+          views.comment_delete, name='comment_delete'),
 ]
