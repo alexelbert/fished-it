707d1116b18b648f460e95f3c013d91356b1be86
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-16 17:07:04 +0100

    Created form for adding catches.

--- catches/forms.py
+++ catches/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Catch
 from django import forms
 
 
@@ -6,3 +6,16 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class CatchForm(forms.ModelForm):
+    class Meta:
+        model = Catch
+        fields = ['fish_species', 'fish_length', 'fish_weight', 'featured_image', 'public']
+        widgets = {
+            'fish_species': forms.TextInput(attrs={'class': 'form-control'}),
+            'fish_length': forms.NumberInput(attrs={'class': 'form-control'}),
+            'fish_weight': forms.NumberInput(attrs={'class': 'form-control'}),
+            'featured_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'public': forms.Select(choices=Catch.STATUS, attrs={'class': 'form-control'}),
+        }
