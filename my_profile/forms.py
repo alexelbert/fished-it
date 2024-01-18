6c24fa5449841e01d4301451f66f06375eb96ee4
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-18 20:38:54 +0100

    Added widgets to the user profile form.

--- my_profile/forms.py
+++ my_profile/forms.py
@@ -5,3 +5,8 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['about_me', 'location', 'profile_picture']
+        widgets = {
+            'about_me': forms.TextInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'profile_picture': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
