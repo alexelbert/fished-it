fc505ea94caeaa14a8cff693c6e95e2cacc4908f
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-18 20:40:14 +0100

    Fixed bug on edit profile function and added success and error messages.

--- my_profile/views.py
+++ my_profile/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
+from django.contrib import messages
+
 
 @login_required
 def profile(request, username):
@@ -10,15 +12,18 @@ def profile(request, username):
 
 
 @login_required
-def edit_profile(request):
+def edit_profile(request, username):
     user_profile = UserProfile.objects.get(user=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
         if form.is_valid():
             form.save()
-            return redirect('my_profile:profile', username=request.user.username)
+            messages.success(request, 'Profile updated successfully.')
+            return redirect('my_profile:profile', username=username)
+        else:
+            messages.error(request, "Uh oh! Profile wasn't updated :(")
     else:
         form = UserProfileForm(instance=user_profile)
 
-    return render(request, 'my_profile/edit_profile.html', {'form': form})
+    return render(request, 'my_profile/edit_profile.html', {'form': form, 'user_profile': user_profile})
