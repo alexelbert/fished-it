d61afae6850d2674ae3f0d3763ed84f983e07ed1
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-18 21:24:32 +0100

    Added url for delete profile.

--- my_profile/urls.py
+++ my_profile/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('<str:username>/', views.profile, name='profile'),
     path('<str:username>/edit_profile/', views.edit_profile, name='edit_profile'),
+    path('my_profile/<str:username>/delete/', views.delete_profile, name='delete_profile'),
 ]
