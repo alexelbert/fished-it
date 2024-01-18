04d24a8aaf8c5ce9b03b052341dd0d59fe1f6935
Alexander Elbert <alexander91elbert@gmail.com>
2024-01-18 20:39:24 +0100

    Added url path to edit profile form.

--- my_profile/urls.py
+++ my_profile/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('<str:username>/', views.profile, name='profile'),
+    path('<str:username>/edit_profile/', views.edit_profile, name='edit_profile'),
 ]
